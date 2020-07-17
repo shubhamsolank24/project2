e914552a471ae4837c4dd047f7781e48eb7c7b57
shubhamsolank24 <67789640+shubhamsolank24@users.noreply.github.com>
2020-07-17 19:21:41 +0530

    Update solution3.py

--- solution3.py
+++ solution3.py
@@ -1,36 +1,32 @@
 import sys
-global huff
+
+def find_frequency(a_string):
+
+    freq_dict = {}
+    freq_list = []
+    for letter in a_string:
+        if letter not in freq_dict:
+            freq_dict[letter] = 1
+        else:
+            freq_dict[letter] += 1
+
+    for key, value in freq_dict.items():
+        freq_list.append((value, key))
+
+    return sorted(freq_list)
 
 def huffman_encoding(data):
-    global huffman
-    huffman = {}
-    for char in data:
-        huffman[char] = huffman.get(char, 0) + 1
-    tree = {}
-    temp = '1'
-    for num in sorted(huffman.items(), key = lambda x: x[1]):
-        tree[num[0]] = temp
-        temp = '0' + temp
+    freq = find_frequency(data)
+    #while list lasts
+    #add slow lowest numbers
+    
 
-    encode = ''
-    for d in data:
-        encode += tree[d]
-    return encode, tree
+
+result = huffman_encoding('good morning')
+
 
 def huffman_decoding(data,tree):
-    huffman = {}
-    for char in tree:
-        huffman[tree[char]] = char
-    #print(huffman)
-    temp = ''
-    decode = ''
-    for d in data:
-        if d == '1':
-            decode += huff[temp + d]
-            temp = ''
-        else:
-            temp += d
-    return decode
+    pass
 
 if __name__ == "__main__":
     codes = {}
@@ -41,12 +37,11 @@ if __name__ == "__main__":
     print ("The content of the data is: {}\n".format(a_great_sentence))
 
     encoded_data, tree = huffman_encoding(a_great_sentence)
-    # print(tree)
-    # print(encoded_data)
+
     print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
     print ("The content of the encoded data is: {}\n".format(encoded_data))
 
     decoded_data = huffman_decoding(encoded_data, tree)
 
     print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
-    print ("The content of the decoded data is: {}\n".format(decoded_data))
+    print ("The content of the encoded data is: {}\n".format(decoded_data))
